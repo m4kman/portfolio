d24508d794f5d8e7a2ead18e90d7b57b282aac8f
m4kman <ihassam187@gmail.com>
2024-04-15 22:52:40 +0500

    add: hero button

--- app/ui/Hero/Hero.tsx
+++ app/ui/Hero/Hero.tsx
@@ -1,10 +1,26 @@
 import * as React from "react";
 import HeroText from "./HeroText";
+import HeroButton from "./HeroButton";
+import Tile from "../Tile";
 
 function Hero() {
   return (
-    <div className="font-sans text-neutral-50">
-      <HeroText />
+    <div className="grid h-screen w-screen place-items-center font-sans text-neutral-50">
+      <section
+        className="absolute grid h-screen w-full grid-cols-[repeat(24,minmax(0,1fr))]
+          overflow-y-clip"
+      >
+        {Array.from(Array(24 * 12), (idx) => (
+          <Tile key={idx} />
+        ))}
+      </section>
+
+      <div className="z-50">
+        <HeroText />
+        <div className="mt-14 bg-inherit text-center">
+          <HeroButton />
+        </div>
+      </div>
     </div>
   );
 }
