4cd25f7108d2bd07bf90f8af1fb74706f7b8fc1f
m4kman <ihassam187@gmail.com>
2024-04-17 21:46:52 +0500

    feat: rework hero

--- app/ui/Hero/Hero.tsx
+++ app/ui/Hero/Hero.tsx
@@ -1,23 +1,57 @@
+"use client";
 import * as React from "react";
+import { motion } from "framer-motion";
+
 import HeroText from "./HeroText";
 import HeroButton from "./HeroButton";
 import Tile from "../Tile";
 
+import range from "@/app/lib/range";
+
 function Hero() {
+  const plane = React.useRef<HTMLDivElement>(null);
+  const maxRotate = 45;
+  const handleMouseMove: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    const x = e.clientX / window.innerWidth;
+    const y = e.clientY / window.innerHeight;
+    const perspective = window.innerWidth * 4;
+    const rotateX = maxRotate * x - maxRotate / 2;
+    const rotateY = (maxRotate * y - maxRotate / 2) * -1;
+    if (plane.current !== null)
+      plane.current.style.transform = `perspective(${perspective}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg)`;
+  };
   return (
-    <div className="grid h-screen w-screen place-items-center font-sans text-neutral-50">
+    <div
+      onMouseMove={(e) => handleMouseMove(e)}
+      className="grid h-screen w-screen place-items-center font-sans text-neutral-50"
+    >
       <section
         className="absolute grid h-screen w-full grid-cols-[repeat(24,minmax(0,1fr))]
           overflow-y-clip"
       >
-        {Array.from(Array(24 * 12), (idx) => (
-          <Tile key={idx} />
+        {range(24 * 12).map((number: number) => (
+          <Tile key={number} />
         ))}
       </section>
 
       <div className="z-50">
-        <HeroText />
-        <div className="mt-14 bg-inherit text-center">
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          ref={plane}
+        >
+          <HeroText />
+        </motion.div>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 100 }}
+          transition={{ duration: 1.75 }}
+          className="mt-6 text-center text-xl font-thin tracking-wide text-[rgb(238,242,255)]"
+        >
+          web developer & security enthusiast
+        </motion.p>
+        <div className="mt-10 text-center">
           <HeroButton />
         </div>
       </div>
--- app/ui/Hero/HeroText.tsx
+++ app/ui/Hero/HeroText.tsx
@@ -1,39 +1,13 @@
-"use client";
 import * as React from "react";
-import { motion } from "framer-motion";
-import BubbleText from "../BubbleText";
+import Text3d from "./Text3d";
 
 export function HeroText() {
   return (
     <div>
-      <motion.div
-        initial={{
-          opacity: 0,
-          y: 100,
-        }}
-        animate={{ opacity: 100, y: 0 }}
-        transition={{
-          duration: 1,
-          stifness: 400,
-          damping: 20,
-          delay: 0.2,
-        }}
-      >
-        <h1 className="pointer-events-auto font-mono text-9xl uppercase tracking-wide">
-          <BubbleText text="Hassam" />
-        </h1>
-        <h1 className="mt-5 text-center font-mono text-9xl uppercase tracking-wide">
-          <BubbleText text="Khan" />
-        </h1>
-      </motion.div>
-      <motion.p
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 100 }}
-        transition={{ duration: 5 }}
-        className="mt-6 text-center text-xl font-thin tracking-wide text-[rgb(238,242,255)]"
-      >
-        web developer & security enthusiast
-      </motion.p>
+      <div className="container flex flex-col justify-center">
+        <Text3d primary="Hassam" secondary="Hassam" />
+        <Text3d primary="Khan" secondary="Khan" />
+      </div>
     </div>
   );
 }
