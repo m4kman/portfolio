1dfbd7730d7b17b72a8bdd3f048194f1f98f4dc5
m4kman <ihassam187@gmail.com>
2024-06-10 15:00:42 +0500

    fix navbar and resume index

--- app/ui/shiny-button.tsx
+++ app/ui/shiny-button.tsx
@@ -26,8 +26,8 @@ const ShinyButton = ({ children }: { children: React.ReactNode }) => {
   return (
     <motion.button
       {...animationProps}
-      className="relative rounded px-6 py-[14px] font-normal backdrop-blur-xl backdrop-filter
-        transition-[box-shadow] duration-300 ease-in-out hover:shadow
+      className="relative z-[99] rounded px-6 py-[14px] font-normal backdrop-blur-xl
+        backdrop-filter transition-[box-shadow] duration-300 ease-in-out hover:shadow
         dark:bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary))_0%,hsl(var(--primary))_60%)]
         dark:hover:shadow-[0_0_20px_hsl(var(--primary)/60%)]"
     >
